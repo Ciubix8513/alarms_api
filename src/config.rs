5f42bb5997322b30ffb31ebb2bb123e2e683799a
Luna <ciubix8514@gmail.com>
2023-08-16 21:45:34 +0300

    Add config file creation and parsing

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,16 @@
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
+
 use serde_derive::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub struct Config {
-    pub data: Vec<ConfigItem>,
+    pub hosts: Vec<ConfigItem>,
+    pub api_key: String,
+    pub ip_address: String,
+    pub port: u16,
 }
 
 #[derive(Serialize, Deserialize, Default, Debug)]
@@ -23,19 +31,36 @@ pub enum Severity {
     Low,
     Middle,
     High,
+    Test,
 }
 
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub enum RepeatingSatus {
+    #[default]
+    NonRepeating,
+    Repeating(u32),
+}
+
+#[derive(Serialize, Deserialize, Default, Debug)]
+pub struct FileArguments {
+    pub path: PathBuf,
+    pub repeating: RepeatingSatus,
+}
 #[derive(Serialize, Deserialize, Default, Debug)]
 pub enum AlarmResponseTypes {
     Sound,
     #[default]
     Log,
+    File(FileArguments),
 }
 
 #[test]
 fn serialization_test() {
     let conf = Config {
-        data: vec![
+        api_key: "123".to_string(),
+        port: 5000,
+        ip_address: "127.0.0.1".to_string(),
+        hosts: vec![
             ConfigItem {
                 name: "Test".to_string(),
                 responses: vec![
@@ -49,7 +74,10 @@ fn serialization_test() {
                     },
                     SeverityItem {
                         severity: Severity::High,
-                        response: AlarmResponseTypes::Sound,
+                        response: AlarmResponseTypes::File(FileArguments {
+                            path: "~/Projects/ed/target/debug/ed".into(),
+                            repeating: RepeatingSatus::Repeating(1000),
+                        }),
                     },
                 ],
             },
@@ -91,4 +119,43 @@ fn serialization_test() {
     };
     let s = serde_yaml::to_string(&conf).unwrap();
     println!("{s}");
+}
+
+pub fn prarse_config(path: PathBuf) -> Result<Config, Box<dyn Error>> {
+    Ok::<Config, Box<dyn Error>>(serde_yaml::from_str(std::str::from_utf8(&std::fs::read(
+        path,
+    )?)?)?)
+}
+pub fn generate_default_config(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config {
+        ip_address: "127.0.0.1".to_string(),
+        port: 5000,
+        api_key: "123".to_string(),
+        hosts: vec![ConfigItem {
+            name: "Host".to_string(),
+            responses: vec![
+                SeverityItem {
+                    severity: Severity::Low,
+                    response: AlarmResponseTypes::Log,
+                },
+                SeverityItem {
+                    severity: Severity::Middle,
+                    response: AlarmResponseTypes::Sound,
+                },
+                SeverityItem {
+                    severity: Severity::High,
+                    response: AlarmResponseTypes::File(FileArguments {
+                        path: "~/test.sh".into(),
+                        repeating: RepeatingSatus::Repeating(1000),
+                    }),
+                },
+            ],
+        }],
+    };
+
+    let contents = serde_yaml::to_string(&config)?;
+
+    std::fs::write(path, contents)?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{
     collections::HashMap,
+    path::PathBuf,
     sync::{mpsc, Arc, Mutex},
 };
 
 use actix_web::{web::Data, App, HttpServer};
 use alarm_endpoint::{alarm, disable_alarm};
+use clap::Parser;
+use config::generate_default_config;
 use once_cell::sync::Lazy;
 
 mod alarm_endpoint;
@@ -14,30 +17,45 @@ mod config;
 pub static CHANNEL_STORE: Lazy<Arc<Mutex<HashMap<u32, mpsc::Sender<()>>>>> =
     Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
 
-#[actix_web::main()]
-async fn main() -> std::io::Result<()> {
-    dotenvy::dotenv().ok();
+#[derive(clap_derive::Parser)]
+struct Args {
+    ///Path to the config file
+    #[arg(short, long, default_value = "./config.yaml")]
+    config_path: PathBuf,
+    ///If set will generate the default config file at the provided config path
+    #[arg(short, long)]
+    generate_config: bool,
+}
 
-    let ip_address = std::env::var("IP_ADDRESS").expect("No ip address provided");
-    let port = std::env::var("PORT")
-        .expect("No port provided")
-        .parse()
-        .expect("Failed to prase port");
-    let api_key = std::env::var("API_KEY").expect("No api key porvided");
+#[actix_web::main()]
+async fn main() {
+    let args = Args::parse();
 
     env_logger::Builder::new()
         .filter_level(log::LevelFilter::Info)
         .format_timestamp(None)
         .init();
 
+    if !args.config_path.exists() {
+        if args.generate_config {
+            generate_default_config(&args.config_path).expect("Unable to generate default config");
+        } else {
+            log::error!("No config file");
+            return;
+        }
+    }
+    let config = config::prarse_config(args.config_path.into()).unwrap();
+
     HttpServer::new(move || {
         App::new()
             .service(alarm)
             .service(disable_alarm)
-            .app_data(Data::new(api_key.clone()))
+            .app_data(Data::new(config.api_key.clone()))
             .wrap(actix_web::middleware::Logger::default())
     })
-    .bind((ip_address, port))?
+    .bind((config.ip_address, config.port))
+    .unwrap()
     .run()
     .await
+    .unwrap();
 }
